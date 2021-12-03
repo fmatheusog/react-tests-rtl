0a9c67a98b89365fc121468a1dbe30be9d43b6a7
Felipe Matheus <fmatheusog@gmail.com>
2021-12-03 16:48:32 -0300

    Requisito 3 - Completo

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,89 @@
-test('', () => {});
+import React from 'react';
+import { Router } from 'react-router-dom';
+import { render, screen } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('Testa o componente FavoritePokemons', () => {
+  const renderWithRouter = (component) => {
+    const history = createMemoryHistory();
+    return { ...render(
+      <Router history={ history }>
+        {component}
+      </Router>,
+    ),
+    history,
+    };
+  };
+
+  const pokemons = [
+    {
+      id: 25,
+      name: 'Pikachu',
+      type: 'Electric',
+      averageWeight: {
+        value: '6.0',
+        measurementUnit: 'kg',
+      },
+      image: 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
+      moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)',
+      foundAt: [
+        {
+          location: 'Kanto Viridian Forest',
+          map: 'https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png',
+        },
+        {
+          location: 'Kanto Power Plant',
+          map: 'https://cdn2.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png',
+        },
+      ],
+      summary: 'This intelligent Pokémon roasts hard berrie'
+        + ' with electricity to make them tender enough to eat.',
+    },
+    {
+      id: 4,
+      name: 'Charmander',
+      type: 'Fire',
+      averageWeight: {
+        value: '8.5',
+        measurementUnit: 'kg',
+      },
+      image: 'https://cdn2.bulbagarden.net/upload/0/0a/Spr_5b_004.png',
+      moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Charmander_(Pok%C3%A9mon)',
+      foundAt: [
+        {
+          location: 'Alola Route 3',
+          map: 'https://cdn2.bulbagarden.net/upload/9/93/Alola_Route_3_Map.png',
+        },
+        {
+          location: 'Kanto Route 3',
+          map: 'https://cdn2.bulbagarden.net/upload/4/4a/Kanto_Route_3_Map.png',
+        },
+        {
+          location: 'Kanto Route 4',
+          map: 'https://cdn2.bulbagarden.net/upload/2/24/Kanto_Route_4_Map.png',
+        },
+        {
+          location: 'Kanto Rock Tunnel',
+          map: 'https://cdn2.bulbagarden.net/upload/6/6f/Kanto_Rock_Tunnel_Map.png',
+        },
+      ],
+      summary: 'The flame on its tail shows the strength of its life force.'
+        + 'If it is weak, the flame also burns weakly.',
+    },
+  ];
+
+  test('Testa se é exibida a mensagem correta quando não há pokemons favoritos', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const message = screen.getByText(/no favorite pokemon found/i);
+    expect(message).toBeInTheDocument();
+  });
+
+  test('Testa se é exibido os pokemons favoritados', () => {
+    renderWithRouter(<FavoritePokemons pokemons={ pokemons } />);
+
+    const pikachu = screen.getByText(/pikachu/i);
+    const charizard = screen.getByText(/charmander/i);
+    expect(pikachu && charizard).toBeInTheDocument();
+  });
+});
